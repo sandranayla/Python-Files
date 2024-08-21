e767f10eda48d26983478c77cdf2f640c5074de9
sandranayla <sandrahlaing1998@gmail.com>
2024-08-21 19:52:31 +0630

    Nothing much. Just singing a song delightfully

---
+++ test.py
@@ -0,0 +1,6 @@
+print('Hello World')
+
+print('Oh six in the mon...ning with no where to go')
+print('Sing hello world if we so good to be old')
+print('Lost in the dark but I never be alone')
+print('Sing hello world if we so good to be old')
